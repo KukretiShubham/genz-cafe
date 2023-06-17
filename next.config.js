28077d9bb1d2606dd0ef79542a42916f8e19f86f
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2023-06-17 10:51:12 +0800

    add: text to landing page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i.ibb.co'],
+        domains: ['i.ibb.co', 
+        'avatars.githubusercontent.com',
+        'upload.wikimedia.org'
+      ],
       },
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,87 +1,86 @@
-
 import Image from 'next/image';
-// import Card from '../components/card';
-// import SideForm from '@/components/sideForm'; // Updated import
-// import { createContext, ContextProvider } from 'react';
-
 
 export default function Home() {
-  // useEffect(() => {
-  //   const getENS = async () => {
-  //     const queryID = 2640064;
-  //     const parameters = [
-  //       QueryParameter.text('Wallet Address', '0x6B2eBFe3FE5c5B84746105421de93Df383b222E8'),
-  //       QueryParameter.enum('Chain', 'ethereum'),
-  //     ];
-  //     try {
-  //       const executionResult = await client.refresh(queryID, parameters);
-  //       console.log(executionResult.result?.rows);
-  //     } catch (error) {
-  //       console.error('Error:', error);
-  //     }
-  //   };
-
-  //   getENS();
-  // }, []); // Run only once on component mount
-
   return (
-<div className="inline-flex flex-col space-y-28 items-center justify-end pt-32 pb-8 bg-black" style={{ width: 1280, height: 3698 }}>
-  <div className="image-container">
-    <Image alt="hero" src="https://i.ibb.co/DbFbCSP/DALL-E-2023-06-16-23-58-21-fill-the-image.png" height={563} width={1280} />
-  </div>
-  <p className="text-8xl text-white" style={{ width: 798, height: 374 }}>
-    World is Crowdy!<br />So will be Virtuality
-  </p>
-  <p className="text-4xl text-white" style={{ width: 798, height: 81 }}>
-    Suite of Perspective rendering Solutions
-  </p>
-  <div className="inline-flex space-x-28 items-center justify-end" style={{ width: 1095, height: 334 }}>
-    <div className="flex items-center justify-center w-80 h-full px-28 py-28 bg-gray-900 shadow rounded-2xl">
-      <p className="flex-1 h-full text-4xl text-white">Genz SDK</p>
+    <div className="flex flex-col items-center justify-end pt-32 pb-8 bg-black">
+      <div className="carousel carousel-end">
+        <div className="carousel-item w-full">
+          <Image
+            alt="hero"
+            src="https://i.ibb.co/DbFbCSP/DALL-E-2023-06-16-23-58-21-fill-the-image.png"
+            height={563}
+            width={1280}
+          />
+        </div>
+      </div>
+      <p className="text-8xl text-white">
+        World is Crowdy!
+        <br />
+        So will be Virtuality
+      </p>
+      <p className="text-4xl text-white">
+        Suite of Perspective rendering Solutions
+      </p>
+      <div className="grid grid-cols-2 gap-x-28 gap-y-8 md:gap-y-28 justify-end w-full max-w-4xl mt-12">
+        <div className="flex items-center justify-center h-full px-8 py-20 bg-gray-900 shadow rounded-2xl">
+          <p className="text-4xl text-center text-white">Genz SDK</p>
+        </div>
+        <p className="text-2xl text-white">
+          An SDK to quickly get wallets compatible with the provided match score.
+        </p>
+        <div className="flex items-center justify-center h-full px-8 py-20 bg-gray-900 shadow rounded-2xl">
+          <p className="text-4xl text-center text-white">Customizable virtual spaces</p>
+        </div>
+        <p className="text-2xl text-white">
+          Virtual Spaces that can be customized according to your preferences.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-x-28 gap-y-8 md:gap-y-28 justify-end w-full max-w-4xl mt-12">
+        <div className="flex items-center justify-center h-80 px-8 py-32 bg-gray-900 shadow rounded-2xl">
+          <p className="text-4xl text-white">genEngine</p>
+        </div>
+        <p className="text-2xl text-white">
+          A powerful perspective rendering engine that can power your game rendering logic
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-end w-full max-w-4xl mt-12">
+        <p className="w-32 h-16 text-4xl text-white">Team</p>
+        <div className="flex flex-row space-x-4 mt-4">
+          <div className="flex flex-col items-center">
+            <Image
+              className="w-40 h-40 rounded-full"
+              src="https://avatars.githubusercontent.com/u/57281769?v=4"
+              alt="team-member"
+              width={288}
+              height={288}
+            />
+            <p className="mt-4 text-2xl text-white">Shubham Kukreti</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              className="w-40 h-40 rounded-full"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/240px-ChatGPT_logo.svg.png"
+              alt="team-member"
+              width={288}
+              height={288}
+            />
+            <p className="mt-4 text-2xl text-center text-white">chatGPT</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              className="w-40 h-40 rounded-full"
+              src="https://upload.wikimedia.org/wikipedia/commons/e/e6/Midjourney_Emblem.png"
+              alt="team-member"
+              width={288}
+              height={288}
+            />
+            <p className="mt-4 text-2xl text-center text-white">Midjourney</p>
+          </div>
+        </div>
+      </div>
+      <p className="text-4xl pt-10 text-center text-white" style={{ width: 1000, height: 147 }}>
+        Building with ❤️
+      </p>
     </div>
-    <p className="text-4xl text-white" style={{ width: 653, height: 297 }}>
-      An SDK to quickly get wallets compatible with the provided match score.
-    </p>
-  </div>
-  <div className="inline-flex space-x-28 items-center justify-end" style={{ width: 1095, height: 334 }}>
-    <div className="flex items-center justify-center w-80 h-full px-8 py-20 bg-gray-900 shadow rounded-2xl">
-      <p className="flex-1 h-full text-4xl text-center text-white">Customizable virtual spaces</p>
-    </div>
-    <p className="text-4xl text-white" style={{ width: 653, height: 297 }}>
-      Virtual Spaces that can be customized according to your preferences.
-    </p>
-  </div>
-  <div className="inline-flex space-x-28 items-end justify-end" style={{ width: 1095, height: 348 }}>
-    <div className="flex items-center justify-center w-80 h-80 px-28 py-32 bg-gray-900 shadow rounded-2xl">
-      <p className="flex-1 h-full text-4xl text-white">SDK</p>
-    </div>
-    <p className="text-4xl text-white" style={{ width: 653, height: 297 }}>
-      Virtual Spaces that can be easily integrated using our SDK.
-    </p>
-  </div>
-  <div className="flex flex-col space-y-16 items-center justify-end" style={{ width: 1146, height: 535 }}>
-  <p className="w-32 h-16 text-4xl text-white">Team</p>
-  <div className="relative flex flex-row" style={{ width: 1146, height: 403 }}>
-    <div className="team-member flex flex-col items-center">
-      <img className="w-72 h-72 rounded-full" src="https://avatars.githubusercontent.com/u/57281769?v=4" alt="team-member" />
-      <p className="mt-4 text-4xl text-white">Shubham Kukreti</p>
-    </div>
-    <div className="team-member flex flex-col items-center">
-      <img className="w-72 h-72 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/240px-ChatGPT_logo.svg.png" alt="team-member" />
-      <p className="mt-4 text-4xl text-center text-white">chatGPT</p>
-    </div>
-    <div className="team-member flex flex-col items-center">
-      <img className="w-72 h-72 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/e/e6/Midjourney_Emblem.png" alt="team-member" />
-      <p className="mt-4 text-4xl text-center text-white">Midjourney</p>
-    </div>
-  </div>
-</div>
-
-  <p className="text-8xl text-center text-white" style={{ width: 1000, height: 147 }}>
-    Building with ♥️
-  </p>
-</div>
-
-
   );
 }
